4386fa06fc4d8f60338f95169e1cb182be75a075
Matjaž Glumac <matjazglumac2ae@gmail.com>
2023-08-26 21:32:17 +0200

    BE - fixed problem with incorrect retry status sent in response

--- backend/src/Enum/SyncRequestStatusEnum.php
+++ backend/src/Enum/SyncRequestStatusEnum.php
@@ -18,7 +18,7 @@ enum SyncRequestStatusEnum
     public static function fromName(string $name): self
     {
         foreach (self::cases() as $status) {
-            if( $name === $status->name or strtoupper($name) ){
+            if( $name === $status->name or strtoupper($name) === $status->name ){
                 return $status;
             }
         }
