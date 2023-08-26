d47b4905f9516f47dba561b2e3a03207bcda7fb2
Matjaž Glumac <matjazglumac2ae@gmail.com>
2023-08-26 12:10:42 +0200

    FE - added backup data for sync chamber (testEntity) data

---
+++ frontend/src/app/packages/mock-data/26-08-2023/sync_testEntity_backup.ts
@@ -0,0 +1,84 @@
+// export default [
+//     {
+//       "key": "801e3b8c-ac2e-4578-ae36-e9705530aa40",
+//       "value": {
+//         "localUUID": "801e3b8c-ac2e-4578-ae36-e9705530aa40",
+//         "changes": [],
+//         "record": {
+//           "id": 83,
+//           "uuid": "801e3b8c-ac2e-4578-ae36-e9705530aa40",
+//           "firstInput": "767",
+//           "secondInput": "yuiy",
+//           "lastModified": "2023-08-25T19:15:31.000Z"
+//         },
+//         "objectStatus": "synced",
+//         "lastModified": "2023-08-25T19:15:31.000Z"
+//       }
+//     },
+//     {
+//       "key": "801e3b8c-ac2e-4578-ae36-e9705530aa49",
+//       "value": {
+//         "localUUID": "801e3b8c-ac2e-4578-ae36-e9705530aa49",
+//         "changes": [],
+//         "record": {
+//           "id": 83,
+//           "uuid": "801e3b8c-ac2e-4578-ae36-e9705530aa49",
+//           "firstInput": "767",
+//           "secondInput": "yuiy",
+//           "lastModified": "2023-08-25T19:15:31.000Z"
+//         },
+//         "objectStatus": "pending_retry",
+//         "lastModified": "2023-08-25T19:15:31.000Z"
+//       }
+//     },
+//     {
+//       "key": "801e3b8c-ac2e-4578-ae36-e9705530aa48",
+//       "value": {
+//         "localUUID": "801e3b8c-ac2e-4578-ae36-e9705530aa48",
+//         "changes": [],
+//         "record": {
+//           "id": 83,
+//           "uuid": "801e3b8c-ac2e-4578-ae36-e9705530aa48",
+//           "firstInput": "767",
+//           "secondInput": "yuiy",
+//           "lastModified": "2023-08-25T19:15:31.000Z"
+//         },
+//         "objectStatus": "pending_retry",
+//         "lastModified": "2023-08-25T19:15:31.000Z"
+//       }
+//     },
+//     {
+//       "key": "801e3b8c-ac2e-4578-ae36-e9705530aa47",
+//       "value": {
+//         "localUUID": "801e3b8c-ac2e-4578-ae36-e9705530aa47",
+//         "changes": [],
+//         "record": {
+//           "id": 83,
+//           "uuid": "801e3b8c-ac2e-4578-ae36-e9705530aa47",
+//           "firstInput": "767",
+//           "secondInput": "yuiy",
+//           "lastModified": "2023-08-25T19:15:31.000Z"
+//         },
+//         "objectStatus": "pending_retry",
+//         "lastModified": "2023-08-25T19:15:31.000Z"
+//       }
+//     },
+//     {
+//       "key": "801e3b8c-ac2e-4578-ae36-e9705530aa46",
+//       "value": {
+//         "localUUID": "801e3b8c-ac2e-4578-ae36-e9705530aa46",
+//         "changes": [],
+//         "record": {
+//           "id": 83,
+//           "uuid": "801e3b8c-ac2e-4578-ae36-e9705530aa46",
+//           "firstInput": "767",
+//           "secondInput": "yuiy",
+//           "lastModified": "2023-08-25T19:15:31.000Z"
+//         },
+//         "objectStatus": "pending_retry",
+//         "lastModified": "2023-08-25T19:15:31.000Z"
+//       }
+//     }
+//   ]
+
+export default {"formatName":"dexie","formatVersion":1,"data":{"databaseName":"sync","databaseVersion":1,"tables":[{"name":"SyncDB_object_name128","schema":"","rowCount":0},{"name":"example_table","schema":"localUUID,changes,lastModified,objectStatus,record","rowCount":0},{"name":"testEntity","schema":"localUUID,changes,lastModified,record,objectStatus","rowCount":5}],"data":[{"tableName":"SyncDB_object_name128","inbound":false,"rows":[]},{"tableName":"example_table","inbound":true,"rows":[]},{"tableName":"testEntity","inbound":true,"rows":[{"localUUID":"801e3b8c-ac2e-4578-ae36-e9705530aa40","changes":[],"record":{"id":83,"uuid":"801e3b8c-ac2e-4578-ae36-e9705530aa40","firstInput":"767","secondInput":"yuiy","lastModified":"2023-08-25T19:15:31.000Z"},"objectStatus":"synced","lastModified":"2023-08-25T19:15:31.000Z","$types":{"changes":"arrayNonindexKeys"}},{"localUUID":"801e3b8c-ac2e-4578-ae36-e9705530aa46","changes":[],"record":{"id":83,"uuid":"801e3b8c-ac2e-4578-ae36-e9705530aa46","firstInput":"767","secondInput":"yuiy","lastModified":"2023-08-25T19:15:31.000Z"},"objectStatus":"pending_retry","lastModified":"2023-08-25T19:15:31.000Z","$types":{"changes":"arrayNonindexKeys"}},{"localUUID":"801e3b8c-ac2e-4578-ae36-e9705530aa47","changes":[],"record":{"id":83,"uuid":"801e3b8c-ac2e-4578-ae36-e9705530aa47","firstInput":"767","secondInput":"yuiy","lastModified":"2023-08-25T19:15:31.000Z"},"objectStatus":"pending_retry","lastModified":"2023-08-25T19:15:31.000Z","$types":{"changes":"arrayNonindexKeys"}},{"localUUID":"801e3b8c-ac2e-4578-ae36-e9705530aa48","changes":[],"record":{"id":83,"uuid":"801e3b8c-ac2e-4578-ae36-e9705530aa48","firstInput":"767","secondInput":"yuiy","lastModified":"2023-08-25T19:15:31.000Z"},"objectStatus":"pending_retry","lastModified":"2023-08-25T19:15:31.000Z","$types":{"changes":"arrayNonindexKeys"}},{"localUUID":"801e3b8c-ac2e-4578-ae36-e9705530aa49","changes":[],"record":{"id":83,"uuid":"801e3b8c-ac2e-4578-ae36-e9705530aa49","firstInput":"767","secondInput":"yuiy","lastModified":"2023-08-25T19:15:31.000Z"},"objectStatus":"pending_retry","lastModified":"2023-08-25T19:15:31.000Z","$types":{"changes":"arrayNonindexKeys"}}]}]}}
